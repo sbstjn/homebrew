f0f659b8b347a4c009bd2c4a59b63268a7f84d96
Sebastian Müller <mail@sbstjn.com>
2017-06-14 20:04:04 +0200

    Create rdm.rb

--- rdm.rb
+++ rdm.rb
@@ -2,6 +2,7 @@ class Rdm < Formula
   desc "Scaffold common GitHub project files like README, COC, and LICENSE"
   homepage "https://github.com/sbstjn/rdm"
   url "https://dl.sbstjn.com/rdm/0.0.7/rdm_darwin_amd64.zip"
+  version "0.0.7"
 
   def install
     bin.install "rdm"
